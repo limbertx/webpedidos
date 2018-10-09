0d2298dbfcd37732dd3f751d2c5fc4ad0fcad44d
limbert yalusqui <limbertx@hotmail.com>
2018-10-08 23:38:00 -0400

    add docker file

--- config/db.php
+++ config/db.php
@@ -1,14 +1,18 @@
 <?php
 
 return [
+	/* mysql */
+	/*
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=dbpedidos',
     'username' => 'root',
     'password' => '123',
-    'charset' => 'utf8',
+    'charset' => 'utf8',*/
 
-    // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
+    /* conexion a postgresql */
+    'class' => 'yii\db\Connection',
+    'dsn'=>'pgsql:host=ec2-54-221-225-11.compute-1.amazonaws.com;port=5432;dbname=d7uik5krjcte5e',
+    'username' => 'ldhjafxnuxsmmt',
+    'password' => 'da39bf88b6b19bf8c27deb592fc8a16087d8988cce2cf62a52fced5b0a52a85d',
+    'charset' => 'utf8',
 ];
