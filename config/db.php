ef85057e1fda5ee5704718f4f5ee4655f21195be
limbert yalusqui <limbertx@hotmail.com>
2018-10-09 23:39:16 -0400

    actualizacion

--- config/db.php
+++ config/db.php
@@ -10,9 +10,19 @@ return [
     'charset' => 'utf8',*/
 
     /* conexion a postgresql */
+/*    'class' => 'yii\db\Connection',
+    'dsn'=>'pgsql:host=localhost;port=5432;dbname=dbpedidos',
+    'username' => 'odoo',
+    'password' => 'odoo',
+    'charset' => 'utf8',
+*/
+    
+    
     'class' => 'yii\db\Connection',
     'dsn'=>'pgsql:host=ec2-54-221-225-11.compute-1.amazonaws.com;port=5432;dbname=d7uik5krjcte5e',
     'username' => 'ldhjafxnuxsmmt',
     'password' => 'da39bf88b6b19bf8c27deb592fc8a16087d8988cce2cf62a52fced5b0a52a85d',
     'charset' => 'utf8',
+
+
 ];
